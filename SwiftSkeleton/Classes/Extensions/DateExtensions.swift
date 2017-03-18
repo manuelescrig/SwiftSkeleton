b5c41dbba4142c68c4d8623f5269ab190c6a39ec
Manuel Escrig <manuelescrig@gmail.com>
2017-03-18 12:13:58 +0100

    Update DateExtensions.swift

--- SwiftSkeleton/Classes/Extensions/DateExtensions.swift
+++ SwiftSkeleton/Classes/Extensions/DateExtensions.swift
@@ -1,1 +1,19 @@
 
+//
+//  Date+Extensions.swift
+//  PhotoFrame
+//
+//  Created by Manuel on 3/18/17.
+//  Copyright © 2017 Manuel. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+ 
+    func toString(format: String = "yyyy-MM-dd HH:mm:ss") -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+}
